bbf0f0ee50eec931c290fc434befc1cd775012bc
Gulina Inga <guingg@inbox.ru>
2019-06-10 16:11:52 +0300

    Updates

---
+++ precache-manifest.ce14bafa912ee8020d1914ad59c789b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858b9624f9205b9e6dae4e3af09bea8b",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "ab7ecd50254c5fce1669",
+    "url": "/to-do-list-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ec87c8d2b034920689e1",
+    "url": "/to-do-list-react/static/css/main.150323ee.chunk.css"
+  },
+  {
+    "revision": "ab7ecd50254c5fce1669",
+    "url": "/to-do-list-react/static/js/2.639b817d.chunk.js"
+  },
+  {
+    "revision": "ec87c8d2b034920689e1",
+    "url": "/to-do-list-react/static/js/main.f430717c.chunk.js"
+  },
+  {
+    "revision": "d0f1dea686286fa7bb4c",
+    "url": "/to-do-list-react/static/js/runtime~main.466cb5a5.js"
+  }
+]);
